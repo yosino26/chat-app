54f9c127d1718ca39f7812453a8ab445d0de025e
yosino26 <fn.yoshino57@gmail.com>
2025-05-25 05:02:48 +0900

    messagesモデルのテストコード１

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'メッセージ投稿' do
+    context 'メッセージが投稿できる場合' do
+      it 'contentとimageが存在していれば保存できる' do
+        expect(@message).to be_valid
+      end
+      it 'contentが空でも保存できる' do
+        @message.content = ''
+        expect(@message).to be_valid
+      end
+      it 'imageが空でも保存できる' do
+        @message.image = nil
+        expect(@message).to be_valid
+      end
+    end
+    context 'メッセージが投稿できない場合' do
+      it 'contentとimageが空では保存できない' do
+      end
+      it 'roomが紐付いていないと保存できない' do
+      end
+      it 'userが紐付いていないと保存できない' do
+
+      end
+    end
+  end
 end
