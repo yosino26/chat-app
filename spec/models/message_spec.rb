861f250c72928c415837957fc30959a7d65a201c
yosino26 <fn.yoshino57@gmail.com>
2025-05-25 05:31:11 +0900

    messagesモデルのテストコード２

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -21,12 +21,21 @@ RSpec.describe Message, type: :model do
     end
     context 'メッセージが投稿できない場合' do
       it 'contentとimageが空では保存できない' do
+        @message.content = ''
+        @message.image = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Content can't be blank")
       end
       it 'roomが紐付いていないと保存できない' do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include('Room must exist')
+      end
       end
       it 'userが紐付いていないと保存できない' do
-
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include('User must exist')
       end
-    end
   end
 end
